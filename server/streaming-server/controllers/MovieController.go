9ee032fe75d17cf0022da712950341894bae030b
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-07 21:36:13 -0500

    Get movies from db and display them using the Movie controller

--- server/streaming-server/controllers/MovieController.go
+++ server/streaming-server/controllers/MovieController.go
@@ -1,11 +1,36 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+
+	"github.com/wesdell/streaming/server/streaming-server/database"
+	"github.com/wesdell/streaming/server/streaming-server/models"
 )
 
+var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
 func GetAllMovies() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "Movies"})
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		var movies []models.Movie
+		cursor, err := movieCollection.Find(ctx, bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies"})
+		}
+		defer cursor.Close(ctx)
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies"})
+		}
+
+		c.JSON(http.StatusOK, movies)
 	}
 }
--- server/streaming-server/main.go
+++ server/streaming-server/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	controller "github.com/wesdell/streaming/server/streaming-server/controllers"
+
+	"github.com/wesdell/streaming/server/streaming-server/controllers"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/movies", controller.GetAllMovies())
+	router.GET("/movies", controllers.GetAllMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server!", err)

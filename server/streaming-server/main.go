d67b8bceb1f3ef5cfed0d88b2d5a8095b0b6cf54
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-07 19:45:24 -0500

    Install gin and run server

--- server/streaming-server/main.go
+++ server/streaming-server/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server!", err)
+	}
 }

1699348ee559e9658c543893b10fa20c82dd4939
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-07 20:26:34 -0500

    Define Movie model with its fields

---
+++ server/streaming-server/models/Movie.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Movie struct {
+	ID          bson.ObjectID `bson:"_id" json:"_id"`
+	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
+	Title       string        `bson:"title" json:"title" validate:"required,min=3,max=150"`
+	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
+	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
+	AdminReview string        `bson:"admin_review" json:"admin_review" validate:"required"`
+	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
+	Genre       []Genre       `bson:"genre" json:"genre" validate:"required,dive"`
+}
+
+type Genre struct {
+	ID   int    `bson:"_id" json:"_id" validate:"required"`
+	Name string `bson:"name" json:"name" validate:"required,min=3,max=100"`
+}
+
+type Ranking struct {
+	Value int    `bson:"value" json:"value" validate:"required"`
+	Name  string `bson:"name" json:"name" validate:"required"`
+}

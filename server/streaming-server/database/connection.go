769028c13db782b32e11b64d0b943ea7445ba5ee
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-07 21:35:20 -0500

    Set up database connection

---
+++ server/streaming-server/database/connection.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+
+	"github.com/wesdell/streaming/server/streaming-server/utils"
+)
+
+var client *mongo.Client = Connect()
+
+func Connect() *mongo.Client {
+	mongodb := utils.GetEnvVariable("MONGODB_URI")
+	if mongodb == "" {
+		log.Fatal("Database connection string not set!")
+	}
+
+	clientOptions := options.Client().ApplyURI(mongodb)
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		return nil
+	}
+	return client
+}
+
+func OpenCollection(collectionName string) *mongo.Collection {
+	dbName := utils.GetEnvVariable("DB_NAME")
+	collection := client.Database(dbName).Collection(collectionName)
+	if collection == nil {
+		return nil
+	}
+	return collection
+}

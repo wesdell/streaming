c7d3990289b894dfd2318788d036efc37513a069
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-08 21:10:42 -0500

    Fix automatically id generation for Movie model

--- server/streaming-server/models/Movie.go
+++ server/streaming-server/models/Movie.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Movie struct {
-	ID          bson.ObjectID `bson:"_id" json:"_id"`
+	ID          bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
 	Title       string        `bson:"title" json:"title" validate:"required,min=3,max=150"`
 	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required,url"`
 	YouTubeID   string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
-	AdminReview string        `bson:"admin_review" json:"admin_review" validate:"required"`
+	AdminReview string        `bson:"admin_review" json:"admin_review"`
 	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
 	Genre       []Genre       `bson:"genre" json:"genre" validate:"required,dive"`
 }

4bf21be5d85c89bcfb91701f11e80d78d96505d6
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-16 11:40:34 -0500

    Refactor and new endpoint for logout from user session

--- server/streaming-server/controllers/UserController.go
+++ server/streaming-server/controllers/UserController.go
@@ -2,12 +2,14 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/wesdell/streaming/server/streaming-server/database"
@@ -15,9 +17,7 @@ import (
 	"github.com/wesdell/streaming/server/streaming-server/utils"
 )
 
-var userCollection = database.OpenCollection("users")
-
-func Register() gin.HandlerFunc {
+func Register(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var user models.User
 		if err := c.ShouldBindJSON(&user); err != nil {
@@ -40,13 +40,15 @@ func Register() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 
-		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		var userCollection = database.OpenCollection("users", client)
+
+		count, err := userCollection.CountDocuments(ctx, bson.D{{Key: "email", Value: user.Email}})
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check existing user"})
 			return
 		}
 		if count > 0 {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+			c.JSON(http.StatusConflict, gin.H{"error": "User already exists"})
 			return
 		}
 
@@ -54,17 +56,18 @@ func Register() gin.HandlerFunc {
 		user.Password = hashedPassword
 		user.CreatedAt = time.Now()
 		user.UpdatedAt = time.Now()
+
 		result, err := userCollection.InsertOne(ctx, user)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
 			return
 		}
 
-		c.JSON(http.StatusCreated, gin.H{"user": result})
+		c.JSON(http.StatusCreated, result)
 	}
 }
 
-func Login() gin.HandlerFunc {
+func Login(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var userLogin models.UserLogin
 		if err := c.ShouldBindJSON(&userLogin); err != nil {
@@ -75,8 +78,10 @@ func Login() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 
+		var userCollection = database.OpenCollection("users", client)
+
 		var existingUser models.User
-		if err := userCollection.FindOne(ctx, bson.M{"email": userLogin.Email}).Decode(&existingUser); err != nil {
+		if err := userCollection.FindOne(ctx, bson.D{{Key: "email", Value: userLogin.Email}}).Decode(&existingUser); err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
 			return
 		}
@@ -91,11 +96,32 @@ func Login() gin.HandlerFunc {
 			return
 		}
 
-		err = utils.UpdateTokens(existingUser.UserId, token, refreshToken)
+		err = utils.UpdateTokens(existingUser.UserId, token, refreshToken, client)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update tokens"})
 			return
 		}
+
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:  "access_token",
+			Value: token,
+			Path:  "/",
+			// Domain:   "localhost",
+			MaxAge:   86400,
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteNoneMode,
+		})
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:  "refresh_token",
+			Value: refreshToken,
+			Path:  "/",
+			// Domain:   "localhost",
+			MaxAge:   604800,
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteNoneMode,
+		})
 
 		c.JSON(http.StatusOK, models.UserResponse{
 			UserId:         existingUser.UserId,
@@ -104,9 +130,75 @@ func Login() gin.HandlerFunc {
 			Role:           existingUser.Role,
 			Email:          existingUser.Email,
 			FavoriteGenres: existingUser.FavoriteGenres,
-			Token:          token,
-			RefreshToken:   refreshToken,
+			//Token:          token,
+			//RefreshToken:   refreshToken,
 		})
+	}
+}
+
+func Logout(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var UserLogout struct {
+			UserId string `json:"user_id"`
+		}
+
+		err := c.ShouldBindJSON(&UserLogout)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+			return
+		}
+
+		fmt.Println("User ID from Logout request:", UserLogout.UserId)
+
+		err = utils.UpdateTokens(UserLogout.UserId, "", "", client)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error logging out"})
+			return
+		}
+
+		// c.SetCookie(
+		// 	"access_token",
+		// 	"",
+		// 	-1, // MaxAge negative → delete immediately
+		// 	"/",
+		// 	"localhost", // Adjust to your domain
+		// 	true,        // Use true in production with HTTPS
+		// 	true,        // HttpOnly
+		// )
+
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:  "access_token",
+			Value: "",
+			Path:  "/",
+			// Domain:   "localhost",
+			MaxAge:   -1,
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteNoneMode,
+		})
+
+		// c.SetCookie(
+		// 	"refresh_token",
+		// 	"",
+		// 	-1,
+		// 	"/",
+		// 	"localhost",
+		// 	true,
+		// 	true,
+		// )
+
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     "refresh_token",
+			Value:    "",
+			Path:     "/",
+			MaxAge:   -1,
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteNoneMode,
+		})
+
+		c.JSON(http.StatusOK, gin.H{"message": "Logged out successfully"})
 	}
 }
 

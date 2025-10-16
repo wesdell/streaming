b7b7ec0794edf46926f4420a204db7c35c692dd4
Sebastian Guerrero <wesdell17@gmail.com>
2025-10-16 11:40:54 -0500

    New endpoint to get all genres from db

---
+++ server/streaming-server/controllers/GenreController.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+
+	"github.com/wesdell/streaming/server/streaming-server/database"
+	"github.com/wesdell/streaming/server/streaming-server/models"
+)
+
+func GetAllGenres(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		var genreCollection = database.OpenCollection("genres", client)
+		cursor, err := genreCollection.Find(ctx, bson.D{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching movie genres"})
+			return
+		}
+		defer cursor.Close(ctx)
+
+		var genres []models.Genre
+		if err := cursor.All(ctx, &genres); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, genres)
+	}
+}

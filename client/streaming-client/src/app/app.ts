f36320182b12a63e9ee7df33e8c54d8a7968219d
Sebastian Guerrero <wesdell17@gmail.com>
2025-11-11 17:34:39 -0500

    Use movies component instead of displaying a singular movie

--- client/streaming-client/src/app/app.ts
+++ client/streaming-client/src/app/app.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
-import { Movie } from './components/movie/movie';
+
+import { Movies } from './components/movies/movies';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [Movie]
+  imports: [Movies]
 })
 
 export class App {
-  movie = {
-    _id: '1',
-    title: 'Interestelar',
-    imdb_id: 'tt0816692',
-    youtube_id: 'zSWdZVtXT7E',
-    poster_path: 'https://image.tmdb.org/t/p/w500/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg',
-    ranking: {
-      ranking_name: 'Top Rated'
+  movies = [
+    {
+      _id: '1',
+      title: 'Interestelar',
+      imdb_id: 'tt0816692',
+      youtube_id: 'zSWdZVtXT7E',
+      poster_path: 'https://image.tmdb.org/t/p/w500/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg',
+      ranking: {
+        ranking_name: 'Top Rated'
+      }
+    },
+    {
+      _id: '2',
+      title: 'Interestelar',
+      imdb_id: 'tt0816692',
+      youtube_id: 'zSWdZVtXT7E',
+      poster_path: 'https://image.tmdb.org/t/p/w500/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg',
+      ranking: {
+        ranking_name: 'Top Rated'
+      }
     }
-  };
+  ];
+
+  message = "No movies to show";
 
   handleReview(imdb: string) {
     console.log('Reviewing movie:', imdb);

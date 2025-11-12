59874fbeac628551147fa71e2ebe325d9b20f277
Sebastian Guerrero <wesdell17@gmail.com>
2025-11-12 17:39:22 -0500

    Use home component on app

--- client/streaming-client/src/app/app.ts
+++ client/streaming-client/src/app/app.ts
@@ -1,41 +1,17 @@
 import { Component } from '@angular/core';
 
-import { Movies } from './components/movies/movies';
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [Movies]
+  imports: [Home]
 })
-
 export class App {
-  movies = [
-    {
-      _id: '1',
-      title: 'Interestelar',
-      imdb_id: 'tt0816692',
-      youtube_id: 'zSWdZVtXT7E',
-      poster_path: 'https://image.tmdb.org/t/p/w500/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg',
-      ranking: {
-        ranking_name: 'Top Rated'
-      }
-    },
-    {
-      _id: '2',
-      title: 'Interestelar',
-      imdb_id: 'tt0816692',
-      youtube_id: 'zSWdZVtXT7E',
-      poster_path: 'https://image.tmdb.org/t/p/w500/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg',
-      ranking: {
-        ranking_name: 'Top Rated'
-      }
-    }
-  ];
-
-  message = "No movies to show";
 
   handleReview(imdb: string) {
-    console.log('Reviewing movie:', imdb);
+    console.log("Reviewing movie:", imdb);
   }
+
 }

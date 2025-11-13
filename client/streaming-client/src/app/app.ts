7d75954f01768d64d424236652afcbf97e9fb1b8
Sebastian Guerrero <wesdell17@gmail.com>
2025-11-13 15:13:59 -0500

    Use header component on app

--- client/streaming-client/src/app/app.ts
+++ client/streaming-client/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { Home } from "./components/home/home";
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [Home]
+  imports: [Home, Header]
 })
 export class App {
 
